526203a26a715e2a9933488999446b300f09a8bb
Swizbiz <swizbizy@gmail.com>
2019-04-09 17:35:59 +0300

    2512

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -7,8 +7,15 @@ public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+        try {
+            uncaughtException(t, e.getCause());
+            System.out.println(e);
+        }catch (NullPointerException ex){
+            t.interrupt();
+        }
     }
 
     public static void main(String[] args) {
+        new Solution().uncaughtException(Thread.currentThread(), new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI"))));
     }
 }
