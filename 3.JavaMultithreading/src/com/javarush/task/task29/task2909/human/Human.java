5e3146bcfccddc252ea322656cefd85bc7a2b324
Swizbiz <swizbizy@gmail.com>
2019-04-12 13:17:01 +0300

    2909-9

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -9,23 +9,20 @@ public class Human implements Alive {
     private int id;
     protected int age;
     protected String name;
+    private BloodGroup bloodGroup;
 
-    protected int[] size;
-    
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    protected Size size;
+    public class Size {
+        public int weight;
+        public int height;
+
+        public Size(int weight, int height) {
+            this.weight = weight;
+            this.height = height;
+        }
+    }
+
     private List<Human> children = new ArrayList<>();
-
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
-    }
-
-    public int getBloodGroup() {
-        return bloodGroup;
-    }
 
     public Human(String name, int age) {
         this.name = name;
@@ -72,8 +69,16 @@ public class Human implements Alive {
         return id;
     }
 
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public BloodGroup getBloodGroup() {
+        return bloodGroup;
+    }
+
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     public void printData() {
