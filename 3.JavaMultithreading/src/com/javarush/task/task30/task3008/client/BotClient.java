c523b8dea4ff8bda7c6cf5d3b28a99936a902d41
Alex <swizbizy@gmail.com>
2019-04-17 17:31:47 +0300

    3008-19

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,5 +1,14 @@
 package com.javarush.task.task30.task3008.client;
 
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
 public class BotClient extends Client {
     @Override
     protected String getUserName() {
@@ -18,7 +27,36 @@ public class BotClient extends Client {
     }
 
     public class BotSocketThread extends SocketThread{
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+            if (!message.contains(": ")) return;
+            String[] messages = message.split(": ");
+            String userName = messages[0];
+            String userMessage = messages[1];
+            HashMap<String, String> formats = new HashMap<String, String>() {
+                {
+                    put("дата", "d.MM.YYYY");
+                    put("день", "d");
+                    put("месяц", "MMMM");
+                    put("год", "YYYY");
+                    put("время", "H:mm:ss");
+                    put("час", "H");
+                    put("минуты", "m");
+                    put("секунды", "s");
+                }
+            };
+            formats.keySet().stream().filter(usMsg -> usMsg.equals(userMessage))
+                    .forEach(s -> sendTextMessage(String.format("Информация для %s: %s",
+                            userName, new SimpleDateFormat(formats.get(s)).format(Calendar.getInstance().getTime())
+                    )));
+            }
     }
 
     public static void main(String[] args) {
