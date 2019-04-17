bec4328b3f15988f6d6093a818f76656abaf81c3
Alex <swizbizy@gmail.com>
2019-04-17 15:36:37 +0300

    3008-15

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -6,14 +6,30 @@ import com.javarush.task.task30.task3008.Message;
 import com.javarush.task.task30.task3008.MessageType;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
     public class SocketThread extends Thread {
+        protected void processIncomingMessage(String message){
+            ConsoleHelper.writeMessage(message);
+        }
 
+        protected void informAboutAddingNewUser(String userName){
+            ConsoleHelper.writeMessage(userName + " connected to chat.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName){
+            ConsoleHelper.writeMessage(userName + " leave the chat");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected){
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this){
+                Client.this.notify();
+            }
+        }
     }
 
     protected String getServerAddress(){
