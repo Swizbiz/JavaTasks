214e7e04be33435cf0176e0e20fcdc356adf3998
Alex <swizbizy@gmail.com>
2019-04-17 14:49:51 +0300

    3008-13

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Client {
     protected Connection connection;
@@ -8,5 +14,35 @@ public class Client {
 
     public class SocketThread extends Thread {
 
+    }
+
+    protected String getServerAddress(){
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Error");
+            clientConnected = false;
+            e.printStackTrace();
+        }
     }
 }
