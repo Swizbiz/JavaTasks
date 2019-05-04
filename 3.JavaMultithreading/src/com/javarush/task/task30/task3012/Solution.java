97975dd366ff552b19c1cb7d8e1ca293c712ce3c
Alex <swizbizy@gmail.com>
2019-05-04 18:46:04 +0300

    3012

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
@@ -12,5 +12,11 @@ public class Solution {
 
     public void createExpression(int number) {
         //напишите тут ваш код
+        StringBuilder result = new StringBuilder().append(number).append(" =");
+        String[] formats = {"", " + %d", " - %d"};
+        for (int i = number, tri = 1; 0 < i; i = ++i / 3, tri *= 3) {
+            result.append(String.format(formats[i % 3], tri));
+        }
+        System.out.println(result);
     }
 }
