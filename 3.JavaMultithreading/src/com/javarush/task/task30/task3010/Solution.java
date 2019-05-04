2a5426af2290d222531b28a98d9d7c3574c3c070
Alex <swizbizy@gmail.com>
2019-05-04 17:07:18 +0300

    3010

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -4,8 +4,24 @@ package com.javarush.task.task30.task3010;
 Минимальное допустимое основание системы счисления
 */
 
+import java.math.BigInteger;
+
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        try {
+            if (args[0].matches("\\w*")){
+                for (int i = 2; i <= 36; i++) {
+                    try {
+                        BigInteger bi = new BigInteger(args[0],i);
+                        System.out.println(i);
+                        break;
+                    } catch (Exception ignored) {
+                    }
+                }
+            }else System.out.println("incorrect");
+        } catch (Exception ignored) {
+
+        }
     }
 }
