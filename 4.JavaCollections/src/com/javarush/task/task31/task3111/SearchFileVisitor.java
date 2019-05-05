a13b4f6f981598944c8e2c7e004e75bfebc412fe
Alex <swizbizy@gmail.com>
2019-05-05 14:21:38 +0300

    3111

--- 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
@@ -6,12 +6,57 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFileVisitor extends SimpleFileVisitor<Path> {
+    private String partOfName;
+    private String partOfContent;
+    private int minSize;
+    private int maxSize;
+    private List<Path> foundFiles = new ArrayList<>();
+
+    public List<Path> getFoundFiles() {
+        return foundFiles;
+    }
+
+    public void setPartOfName(String partOfName) {
+        this.partOfName = partOfName;
+    }
+
+    public void setPartOfContent(String partOfContent) {
+        this.partOfContent = partOfContent;
+    }
+
+    public void setMinSize(int minSize) {
+        this.minSize = minSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        byte[] content = Files.readAllBytes(file); // размер файла: content.length
+        boolean containsName = true;
+        if (partOfName != null && !file.getFileName().toString().contains(partOfName))
+            containsName = false;
+
+        String content = new String(Files.readAllBytes(file)); //read the contents of the file
+        boolean containsContent = true;
+        if (partOfContent != null && !content.contains(partOfContent))
+            containsContent = false;
+
+        boolean moreMinSize = true;
+        if (minSize != 0 && content.length() < minSize)
+            moreMinSize = false;
+
+        boolean lessMaxSize = true;
+        if (maxSize != 0 && content.length() > maxSize)
+            lessMaxSize = false;
+
+        if (containsName && containsContent && moreMinSize && lessMaxSize)
+            foundFiles.add(file);
 
         return super.visitFile(file, attrs);
     }
