7f189ce214791581e8b252a85bcb6eeb9c3a113e
Alex <swizbizy@gmail.com>
2019-05-05 12:17:18 +0300

    3102

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,18 +1,29 @@
-package com.javarush.task.task31.task3102;
+ package com.javarush.task.task31.task3102;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-/* 
-Находим все файлы
-*/
+ /*
+ Находим все файлы
+ */
 public class Solution {
     public static List<String> getFileTree(String root) throws IOException {
-        return null;
+        List<String> list = Files.walk(Paths.get(root),Integer.MAX_VALUE).map(Path::toFile).filter(File::isFile).map(File::getAbsolutePath).collect(Collectors.toList());
+        return list;
 
     }
 
     public static void main(String[] args) {
-        
+        try {
+            System.out.println(getFileTree("C:\\11"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
