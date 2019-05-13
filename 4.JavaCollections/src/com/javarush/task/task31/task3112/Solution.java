6bc6f169ef69a3f9693bc41995cf52158d5bea3e
Alex <swizbizy@gmail.com>
2019-05-13 12:08:37 +0300

    3112

--- 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task31.task3112;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /* 
 Загрузчик файлов
@@ -12,7 +14,7 @@ import java.nio.file.Paths;
 public class Solution {
 
     public static void main(String[] args) throws IOException {
-        Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt", Paths.get("D:/MyDownloads"));
+        Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt", Paths.get("C:\\11\\git"));
 
         for (String line : Files.readAllLines(passwords)) {
             System.out.println(line);
@@ -20,6 +22,11 @@ public class Solution {
     }
 
     public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
-        // implement this method
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openStream();
+        Path tempFile = Files.createTempFile("temp", ".tmp");
+        Files.copy(inputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        Path downloadFile = Paths.get(downloadDirectory.toString(), Paths.get(url.getFile()).getFileName().toString());
+        return Files.move(tempFile, downloadFile, StandardCopyOption.REPLACE_EXISTING);
     }
 }
