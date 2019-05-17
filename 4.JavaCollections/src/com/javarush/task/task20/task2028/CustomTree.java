2eb7bec17885b2ca8c851bb38043c09cc2865b65
Alex <swizbizy@gmail.com>
2019-05-17 22:49:26 +0300

    3109-4

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,14 +1,19 @@
 package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /* 
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+    Entry<String> root;
+    private List<Entry<String>> entryList = new ArrayList<>();
+
+    public CustomTree() {
+        this.root = new Entry<>("0");
+        entryList.add(root);
+    }
 
     static class Entry<T> implements Serializable {
         String elementName;
@@ -21,9 +26,39 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
             this.availableToAddRightChildren = true;
         }
 
-        public boolean isAvailableToAddChildren(){
+        public boolean isAvailableToAddChildren() {
             return availableToAddLeftChildren | availableToAddRightChildren;
         }
+    }
+
+    @Override
+    public boolean add(String s) {
+        Entry<String> newEntry = new CustomTree.Entry<>(s);
+        for (Entry<String> entry : entryList) {
+            if (entry.isAvailableToAddChildren()) {
+                if (entry.availableToAddLeftChildren) {
+                    entry.availableToAddLeftChildren = false;
+                    entry.leftChild = newEntry;
+                    newEntry.parent = entry;
+                } else if (entry.availableToAddRightChildren) {
+                    entry.availableToAddRightChildren = false;
+                    entry.rightChild = newEntry;
+                    newEntry.parent = entry;
+                }
+                break;
+            }
+        }
+        entryList.add(newEntry);
+        return true;
+    }
+
+    public String getParent(String s) {
+        String parent = null;
+        for (Entry<String> entry : entryList) {
+            if (entry.elementName.equals(s))
+                parent = entry.parent.elementName;
+        }
+        return parent;
     }
 
     @Override
@@ -63,6 +98,6 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public int size() {
-        return 0;
+        return entryList.size() - 1;
     }
 }
