a182255da4f35e3d3ff20191792fd4b140fd18f2
Alex <swizbizy@gmail.com>
2019-05-17 09:59:05 +0300

    3109

--- 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -1,24 +1,32 @@
 package com.javarush.task.task31.task3109;
 
+import java.io.FileInputStream;
 import java.util.Properties;
 
-/* 
+/*
 Читаем конфиги
 */
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        Properties properties = solution.getProperties("4.JavaCollections\\src\\com\\javarush\\task\\task31\\task3109\\properties.xml");
         properties.list(System.out);
 
-        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties = solution.getProperties("4.JavaCollections\\src\\com\\javarush\\task\\task31\\task3109\\properties.txt");
         properties.list(System.out);
 
-        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties = solution.getProperties("4.JavaCollections\\src\\com\\javarush\\task\\task31\\task3109\\notExists");
         properties.list(System.out);
     }
 
     public Properties getProperties(String fileName) {
-        return null;
+        Properties properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream(fileName)){
+            if (fileName.endsWith(".xml"))
+                properties.loadFromXML(fileInputStream);
+            properties.load(fileInputStream);
+        } finally {
+            return properties;
+        }
     }
 }
