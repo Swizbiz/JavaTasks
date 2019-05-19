a866507f59271a00b4ddabce24b3720c59ae2f90
Alex <swizbizy@gmail.com>
2019-05-19 13:00:56 +0300

    3210

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task32.task3210;
 
-import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Arrays;
 
 /* 
 Используем RandomAccessFile
@@ -9,6 +9,19 @@ import java.io.RandomAccessFile;
 
 public class Solution {
     public static void main(String... args) {
+        try {
+            RandomAccessFile randomAccessFile = new RandomAccessFile(args[0], "rw");
+            randomAccessFile.seek(Integer.parseInt(args[1]));
 
+            byte[] bytes = new byte[args[2].length()];
+            randomAccessFile.read(bytes, 0, args[2].length());
+
+            randomAccessFile.seek(randomAccessFile.length());
+            if (Arrays.equals(bytes, args[2].getBytes()))
+                randomAccessFile.write("true".getBytes());
+            else randomAccessFile.write("false".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
