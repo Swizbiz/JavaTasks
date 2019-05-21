017ddbb19cad5dfd87bd9f990e1d098cf3e5818d
Alex <swizbizy@gmail.com>
2019-05-21 22:05:49 +0300

    3303

--- 4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
@@ -1,13 +1,17 @@
 package com.javarush.task.task33.task3303;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 
 /* 
 Десериализация JSON объекта
 */
 public class Solution {
     public static <T> T convertFromJsonToNormal(String fileName, Class<T> clazz) throws IOException {
-        return null;
+        return new ObjectMapper().readValue(new File(fileName), clazz);
     }
 
     public static void main(String[] args) {
