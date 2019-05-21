10411ae94ae70e471fb09c70966912a924136917
Alex <swizbizy@gmail.com>
2019-05-21 22:28:45 +0300

    3304

--- 4.JavaCollections/src/com/javarush/task/task33/task3304/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3304/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task33.task3304;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -18,7 +19,10 @@ public class Solution {
     }
 
     public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException {
-        return null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
+        String jsonString = objectMapper.writeValueAsString(one);
+        return objectMapper.readValue (jsonString, resultClassObject);
     }
 
     @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
