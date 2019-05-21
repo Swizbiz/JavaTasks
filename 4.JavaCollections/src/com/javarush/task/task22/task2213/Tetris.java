7f4433c39f86201887eeb946bc12ea8c9929337a
Alex <swizbizy@gmail.com>
2019-05-21 14:30:46 +0300

    3212-3

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,8 +3,16 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Figure figure;
     private Field field;
-
+    
     public static void main(String[] args) {
 
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public Field getField() {
+        return field;
     }
 }
