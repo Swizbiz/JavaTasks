5468244c6d6cf988bee8c2dfbd6ef85491c33fad
Alex <swizbizy@gmail.com>
2019-05-21 08:58:33 +0300

    3110

--- 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task32.task3204;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 /* 
 Генератор паролей
@@ -12,6 +15,23 @@ public class Solution {
     }
 
     public static ByteArrayOutputStream getPassword() {
-        return null;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(8);
+        Random rand = new Random();
+        String password = "";
+        while (true) {
+            password = rand.ints(48, 123)
+                    .filter(i -> (i <= 57 || (i >= 65 && i <= 90) || i >= 97))
+                    .limit(8)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+            if (password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$"))
+                break;
+        }
+        try {
+            byteArrayOutputStream.write(password.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return byteArrayOutputStream;
     }
 }
