64115b691bf15ca79652098f03928bf13ad40b0a
Alex <swizbizy@gmail.com>
2019-05-23 17:52:26 +0300

    3309

--- 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -1,11 +1,57 @@
 package com.javarush.task.task33.task3309;
 
-/* 
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+
+/*
 Комментарий внутри xml
 */
 public class Solution {
     public static String toXmlWithComment(Object obj, String tagName, String comment) {
-        return null;
+        StringWriter write = new StringWriter();
+        try {
+            JAXBContext context = JAXBContext.newInstance(obj.getClass());
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setCoalescing(true);
+            DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+            marshaller.marshal(obj, document);
+            NodeList nodeList = document.getElementsByTagName("*");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeName().equals(tagName))
+                    node.getParentNode().insertBefore(document.createComment(comment), node);
+                if (node.getFirstChild().getNodeType() == node.TEXT_NODE && node.getFirstChild().getTextContent().matches(".*[<>&\'\"].*")) {
+                    CDATASection cdataSection = document.createCDATASection(node.getFirstChild().getTextContent());
+                    node.replaceChild(cdataSection, node.getFirstChild());
+                }
+            }
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");  // this allows you make \n
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.STANDALONE, "no");
+            transformer.transform(new DOMSource(document), new StreamResult(write));
+        } catch (JAXBException | ParserConfigurationException | TransformerException e) {
+            e.printStackTrace();
+        }
+
+        return write.toString();
     }
 
     public static void main(String[] args) {
