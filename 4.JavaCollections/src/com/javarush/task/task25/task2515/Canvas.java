9d9328a995b00216b3f556ecf59035732133d3b6
Alex <swizbizy@gmail.com>
2019-05-23 22:18:37 +0300

    2515-10

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task25.task2515;
 
-import java.util.Arrays;
 
 public class Canvas {
     private int width;
@@ -10,41 +9,47 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        this.matrix = new char[height][width];
+        this.matrix = new char[height + 2][width + 2];
     }
 
     public void clear() {
-        for (int i = 0; i < matrix.length; i++) {
-            Arrays.fill(matrix[i], ' ');
-        }
-    }
-
-    public void print() {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println();
-    }
-
-    public void setPoint(double x, double y, char c) {
-        if (x < 0 || y < 0 || y >= matrix[0].length || x >= matrix[0].length)
-            return;
-        int roundX = (int) Math.round(x);
-        int roundY = (int) Math.round(y);
-        matrix[roundY][roundX] = c;
+        this.matrix = new char[height + 2][width + 2];
     }
 
     public void drawMatrix(double x, double y, int[][] matrix, char c) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] != 0)
+                if (matrix[i][j] != 0) {
                     setPoint(x + j, y + i, c);
+                }
             }
         }
+    }
+
+    public void setPoint(double x, double y, char c) {
+        int xRounded = (int) Math.round(x);
+        int yRounded = (int) Math.round(y);
+        if (xRounded >= 0 && xRounded < matrix[0].length && yRounded >= 0 && yRounded < matrix.length) {
+            matrix[yRounded][xRounded] = c;
+        }
+    }
+
+    public void print() {
+        System.out.println();
+
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
     public int getWidth() {
