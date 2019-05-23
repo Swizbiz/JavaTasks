a98aafd574f5d14cbaf4103871ac143cb6078cee
Alex <swizbizy@gmail.com>
2019-05-23 22:37:21 +0300

    2515-13

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -14,4 +14,21 @@ public class SpaceShip extends BaseObject {
     public void moveRight() {
         this.dx = 1.0;
     }
+
+    @Override
+    public void move() {
+        x += dx;
+        checkBorders(0, Space.game.getWidth(), 0, Space.game.getHeight());
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+    }
+
+    public void fire() {
+        Rocket rocket1 = new Rocket(x - 2.0, y);
+        Rocket rocket2 = new Rocket(x + 2.0, y);
+        Space.game.getRockets().add(rocket1);
+        Space.game.getRockets().add(rocket2);
+    }
 }
