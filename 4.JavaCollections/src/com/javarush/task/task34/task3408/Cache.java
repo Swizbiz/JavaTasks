f8d6f6582557aed5045e2f67de8d9d313e0cc734
Alex <swizbizy@gmail.com>
2019-05-24 16:19:13 +0300

    3408

--- 4.JavaCollections/src/com/javarush/task/task34/task3408/Cache.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3408/Cache.java
@@ -1,18 +1,35 @@
 package com.javarush.task.task34.task3408;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 public class Cache<K, V> {
-    private Map<K, V> cache = null;   //TODO add your code here
+    private Map<K, V> cache = new WeakHashMap<>();
 
     public V getByKey(K key, Class<V> clazz) throws Exception {
-        //TODO add your code here
-        return null;
+        Object obj = cache.get(key);
+        if (obj == null) {
+           Constructor constructor = clazz.getConstructor(key.getClass());
+           Object newObj = constructor.newInstance(key);
+           put((V) newObj);
+           return (V) newObj;
+        }
+        return (V) obj;
     }
 
     public boolean put(V obj) {
-        //TODO add your code here
-        return false;
+        try {
+            Class clazz = Class.forName(obj.getClass().getName());
+            Method method = clazz.getDeclaredMethod("getKey");
+            method.setAccessible(true);
+            cache.put((K) method.invoke(obj), obj);
+            return true;
+        } catch (Exception ignore){
+            return false;
+        }
     }
 
     public int size() {
