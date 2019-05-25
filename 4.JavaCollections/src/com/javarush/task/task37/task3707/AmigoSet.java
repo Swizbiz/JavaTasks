25df11ced601ae2978c6d18bdc9bbd06e8bb3e55
Alex <swizbizy@gmail.com>
2019-05-25 17:42:25 +0300

    3707-5

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
@@ -12,10 +15,60 @@ public class AmigoSet<E> extends AbstractSet implements Set, Serializable, Clone
     }
 
     public AmigoSet(Collection<? extends E> collection) {
-        this.map = new HashMap<>(Math.max(16, (int)(collection.size()/.75f)+1));
-        for (E e: collection){
+        this.map = new HashMap<>(Math.max(16, (int) (collection.size() / .75f) + 1));
+        for (E e : collection) {
             this.map.put(e, PRESENT);
         }
+    }
+
+    private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
+        // Read in any hidden serialization magic
+        objectInputStream.defaultReadObject();
+
+        //Read capacity, load factor and size
+        int capacity = objectInputStream.readInt();
+        float loadFactor = objectInputStream.readFloat();
+        int size = objectInputStream.readInt();
+
+        // Create backing HashMap
+        map = new HashMap<E, Object>(capacity, loadFactor);
+
+        // Read in all elements in the proper order.
+        for (int i = 0; i < size; i++) {
+            E e = (E) objectInputStream.readObject();
+            map.put(e, PRESENT);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AmigoSet)) return false;
+        if (!super.equals(o)) return false;
+        AmigoSet<?> amigoSet = (AmigoSet<?>) o;
+        return Objects.equals(map, amigoSet.map);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), map);
+    }
+
+    private void writeObject(ObjectOutputStream stream) throws IOException {
+        // Write out any hidden serialization magic
+        stream.defaultWriteObject();
+
+        // Write out HashMap capacity, load factor and size
+        int capacity = HashMapReflectionHelper.callHiddenMethod(map, "capacity");
+        stream.writeInt(capacity);
+        float loadFactor = HashMapReflectionHelper.callHiddenMethod(map, "loadFactor");
+        stream.writeFloat(loadFactor);
+        stream.writeInt(map.size());
+
+        // Write out all elements in the proper order.
+        for (E e : map.keySet())
+            stream.writeObject(e);
+
     }
 
     @Override
@@ -46,7 +99,7 @@ public class AmigoSet<E> extends AbstractSet implements Set, Serializable, Clone
     @Override
     public Object clone() {
         try {
-            AmigoSet clone = (AmigoSet)super.clone();
+            AmigoSet clone = (AmigoSet) super.clone();
             clone.map = (HashMap) map.clone();
             return clone;
         } catch (Exception e) {
