9def4264ee1b8bfc5628feb6c79a798e0ebd0356
Alex <swizbizy@gmail.com>
2019-05-28 10:56:17 +0300

    3110-4

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -7,7 +9,7 @@ import java.nio.file.Paths;
 import static java.lang.System.in;
 
 public class Archiver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
             System.out.println("Введите путь к архиву");
             String zipFilePath = br.readLine();
@@ -19,5 +21,7 @@ public class Archiver {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        ExitCommand exitCommand = new ExitCommand();
+        exitCommand.execute();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -4,22 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import static java.lang.System.in;
-
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
     public static void writeMessage(String message) {
         System.out.println(message);
     }
 
     public static String readString() throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            return br.readLine();
-        }
+        String text = bis.readLine();
+        return text;
     }
 
     public static int readInt() throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            return Integer.parseInt(br.readLine());
-        }
+        String text = readString();
+        return Integer.parseInt(text.trim());
     }
 }
