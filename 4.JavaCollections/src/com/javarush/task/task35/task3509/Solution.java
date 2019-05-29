8e046774e1bcbb2544c1699ce958f43e385fce3c
Alex <swizbizy@gmail.com>
2019-05-29 13:40:43 +0300

    3509

--- 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -11,18 +11,25 @@ public class Solution {
     public static void main(String[] args) {
     }
 
-    public static ArrayList newArrayList(Object... elements) {
-        //напишите тут ваш код
-        return null
+    public static <T> ArrayList<T> newArrayList(T... elements) {
+        ArrayList<T> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(elements));
+        return arrayList;
     }
 
-    public static HashSet newHashSet(Object... elements) {
-        //напишите тут ваш код
-        return null
+    public static <T> HashSet<T> newHashSet(T... elements) {
+        HashSet<T> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList(elements));
+        return hashSet;
     }
 
-    public static HashMap newHashMap(List keys, List values) {
-        //напишите тут ваш код
-        return null;
+    public static <K, V> HashMap<K, V> newHashMap(List<? extends K> keys, List<? extends V> values) {
+        if (keys.size() != values.size())
+            throw new IllegalArgumentException();
+        HashMap<K, V> hashMap = new HashMap<>();
+        for (int i = 0; i < keys.size(); i++) {
+            hashMap.put(keys.get(i), values.get(i));
+        }
+        return hashMap;
     }
 }
