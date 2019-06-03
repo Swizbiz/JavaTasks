3041ba5a9fe188c66e0c4a81a9d052917521696f
Alex <swizbizy@gmail.com>
2019-06-03 17:57:49 +0300

    3605

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task36.task3605;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
 import java.util.TreeSet;
 
 /* 
@@ -10,5 +14,15 @@ import java.util.TreeSet;
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
+        Set<Character> treeSet = new TreeSet<>();
+        Files.lines(Paths.get(args[0]))
+                .map(String::toLowerCase)
+                .forEach(s -> s.chars()
+                        .mapToObj(i -> (char) i)
+                        .filter(character -> character > 96 && character < 123)
+                        .forEach(treeSet::add));
+        treeSet.stream()
+                .limit(5)
+                .forEach(System.out::print);
     }
 }
