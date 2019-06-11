e54b8778e20d2e449520f783e62e8762ee237a08
Alex <swizbizy@gmail.com>
2019-06-12 00:11:14 +0300

    3310-1

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task33.task3310;
+
+public class Shortener {
+    public Long getId(String string) {
+        return 0L;
+    }
+
+    public String getString(Long id) {
+        return "";
+    }
+}
